3ca8095937867d7f381dfb1a134211266bf00657
mmulyana <mulyan.t20@gmail.com>
2024-07-18 03:45:24 +0700

    feat: add education form

--- src/routes/dashboard/cv/component/control/form.tsx
+++ src/routes/dashboard/cv/component/control/form.tsx
@@ -8,13 +8,14 @@ import {
 } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
 import { zodResolver } from '@hookform/resolvers/zod'
-import { useForm } from 'react-hook-form'
-import { basicSchema } from './schema'
+import { useFieldArray, useForm } from 'react-hook-form'
+import { basicSchema, educationSchema } from './schema'
 import { z } from 'zod'
 import { useAtom } from 'jotai'
 import { resumeAtom } from '../..'
 import { useEffect } from 'react'
 import { Button } from '@/components/ui/button'
+import { Textarea } from '@/components/ui/textarea'
 
 export function BasicForm() {
   const form = useForm<z.infer<typeof basicSchema>>({
@@ -161,6 +162,149 @@ export function BasicForm() {
             </FormItem>
           )}
         />
+        <Button type='submit' className='mt-2'>
+          Save
+        </Button>
+      </form>
+    </Form>
+  )
+}
+
+export function EducationForm() {
+  const form = useForm<z.infer<typeof educationSchema>>({
+    resolver: zodResolver(educationSchema),
+    defaultValues: {
+      educations: [
+        {
+          title: '',
+          school: '',
+          description: '',
+          start_date: '',
+          end_date: '',
+        },
+      ],
+    },
+  })
+
+  const { fields, append, remove } = useFieldArray({
+    name: 'educations',
+    control: form.control,
+  })
+
+  // const [resume, setResume] = useAtom(resumeAtom)
+  // useEffect(() => {
+  //   const data = form.watch()
+  //   const subscription = form.watch((data: Record<string, string>) => {
+  //     setResume({ ...resume, ...data })
+  //   })
+  //   return () => subscription.unsubscribe()
+  // }, [form.watch])
+
+  const onSubmit = (data: z.infer<typeof educationSchema>) => {
+    console.log(data)
+  }
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className='flex flex-col gap-4 px-3'
+      >
+        <FormLabel className='text-xs text-gray-400'>Educations</FormLabel>
+        {fields.map((field, index) => (
+          <div key={index} className='flex flex-col gap-2'>
+            <div>
+              <FormLabel
+                htmlFor={`educations.${index}.title`}
+                className='text-xs font-normal text-gray-500'
+              >
+                Title
+              </FormLabel>
+              <Input
+                placeholder='title'
+                {...form.register(`educations.${index}.title`)}
+              />
+            </div>
+            <div>
+              <FormLabel
+                htmlFor={`educations.${index}.school`}
+                className='text-xs font-normal text-gray-500'
+              >
+                School
+              </FormLabel>
+              <Input
+                placeholder='school'
+                {...form.register(`educations.${index}.school`)}
+              />
+            </div>
+            <div>
+              <FormLabel
+                htmlFor={`educations.${index}.description`}
+                className='text-xs font-normal text-gray-500'
+              >
+                Description
+              </FormLabel>
+              <Textarea
+                placeholder='description'
+                {...form.register(`educations.${index}.description`)}
+              />
+            </div>
+            <div className='grid grid-cols-2 gap-4'>
+              <div>
+                <FormLabel
+                  htmlFor={`educations.${index}.start_date`}
+                  className='text-xs font-normal text-gray-500'
+                >
+                  Start Date
+                </FormLabel>
+                <Input
+                  type='date'
+                  className='w-full'
+                  {...form.register(`educations.${index}.start_date`)}
+                />
+              </div>
+              <div>
+                <FormLabel
+                  htmlFor={`educations.${index}.end_date`}
+                  className='text-xs font-normal text-gray-500'
+                >
+                  End Date
+                </FormLabel>
+                <Input
+                  className='w-full'
+                  type='date'
+                  {...form.register(`educations.${index}.end_date`)}
+                />
+              </div>
+            </div>
+            <Button
+              type='button'
+              variant='ghost'
+              size='sm'
+              onClick={() => remove(index)}
+            >
+              <span className='text-xs'>Hapus</span>
+            </Button>
+          </div>
+        ))}
+        <div>
+          <Button
+            type='button'
+            variant='secondary'
+            size='sm'
+            onClick={() =>
+              append({
+                title: '',
+                description: '',
+                school: '',
+                start_date: '',
+                end_date: '',
+              })
+            }
+          >
+            Tambah
+          </Button>
+        </div>
         <Button type='submit' className='mt-2'>
           Save
         </Button>

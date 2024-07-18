6fe644b94083b63b9e4174c27726b9ed9003b213
mmulyana <mulyan.t20@gmail.com>
2024-07-18 21:21:54 +0700

    feat: add type Resume

--- src/routes/dashboard/cv/index.tsx
+++ src/routes/dashboard/cv/index.tsx
@@ -2,7 +2,54 @@ import useUrlState from '@ahooksjs/use-url-state'
 import { EditResume, Resume } from './component'
 import { atom } from 'jotai'
 
-export const resumeAtom = atom<Record<string, string>>({})
+type Experiences = {
+  title: string
+  company: string
+  description: string
+  start_date: string
+  end_date: string
+  link: string
+}
+
+type Educations = {
+  title: string
+  school: string
+  description: string
+  start_date: string
+  end_date: string
+}
+
+type Portfolios = {
+  title: string
+  role: string
+  description: string
+  start_date: string
+  end_date: string
+  link: string
+}
+
+type Skills = {
+  id: number
+  name: string
+}
+
+type Resume = {
+  design: number
+  name: string
+  photo: string
+  description: string
+  address: string
+  email: string
+  portfolio: string
+  linkedin: string
+  phone: string
+  experience: Experiences[]
+  educations: Educations[]
+  portfolios: Portfolios[]
+  skills: Skills[]
+}
+
+export const resumeAtom = atom<Partial<Resume>>({})
 
 export default function Page() {
   const [url, _] = useUrlState({

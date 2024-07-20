7820357476d0da6fb0f9e436c71cada1f15a1e8c
mmulyana <mulyan.t20@gmail.com>
2024-07-20 20:38:05 +0700

    feat: add toaster

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import Routers from './routes'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { Toaster } from './components/ui/sonner'
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -17,6 +18,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <Routers />
+      <Toaster richColors theme='light'/>
     </QueryClientProvider>
   </React.StrictMode>
 )

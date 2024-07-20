1753c878a37d8c155d6abf7d37d7858a862e52ab
mmulyana <mulyan.t20@gmail.com>
2024-07-20 20:38:45 +0700

    feat: urls constant

---
+++ src/constant/_urls.ts
@@ -0,0 +1,8 @@
+const BASE_URL = 'https://resumebuilder.hqhan.my.id'
+
+export const URLS = {
+  LOGIN: BASE_URL + '/auth/login',
+  REGISTER: BASE_URL + '/auth/register',
+  RESUME: BASE_URL + '/resume',
+  SKILLS: BASE_URL + '/skills',
+}

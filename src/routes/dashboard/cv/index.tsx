439626102f573e2453a4266d8f88821c57e8d179
mmulyana <mulyan.t20@gmail.com>
2024-08-04 22:20:35 +0700

    fix: resume atom at first render

--- src/routes/dashboard/cv/index.tsx
+++ src/routes/dashboard/cv/index.tsx
@@ -2,15 +2,24 @@ import { Resume as ResumeTypes } from '@/types/resume'
 import useUrlState from '@ahooksjs/use-url-state'
 import { EditResume, Resume } from './component'
 import { atomWithStorage } from 'jotai/utils'
-import { atom } from 'jotai'
+import { useAtom } from 'jotai'
+import { useEffect } from 'react'
 
 export const resumeAtom = atomWithStorage<ResumeTypes | null>('resume', null)
-export const isResumeChangedAtom = atom(false)
 
 export default function Page() {
   const [url, _] = useUrlState({
     id: '',
   })
+
+  const [resume, setResume] = useAtom(resumeAtom)
+
+  useEffect(() => {
+    if (resume !== null) return
+    if (resume == null) {
+      setResume((prev) => ({ ...prev, design: '1', skills: [] }))
+    }
+  }, [])
 
   if (url.id !== '') return <EditResume />
 

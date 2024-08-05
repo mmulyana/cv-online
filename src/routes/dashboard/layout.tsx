02bdbedea43ae45711577fb06b54dae8fa6c566f
mmulyana <mulyan.t20@gmail.com>
2024-08-05 23:01:36 +0700

    feat: apply modal

--- src/routes/dashboard/layout.tsx
+++ src/routes/dashboard/layout.tsx
@@ -24,19 +24,21 @@ import { PATHS } from '@/constant/_paths'
 import { userAtom } from '@/atoms/user'
 import { useAtomValue } from 'jotai'
 import { cn } from '@/utils/cn'
+import { DeleteModal, PublishModal } from './modal'
 
 export default function Layout({
   children,
   className,
 }: React.PropsWithChildren & { className?: string }) {
   const [isOpen, setIsOpen] = useState(false)
-  const navigate = useNavigate()
 
   return (
     <>
       <Navbar setOpen={setIsOpen} />
       <div className={cn('px-4 md:px-6 pt-16', className)}>{children}</div>
       <Profile open={isOpen} setOpen={setIsOpen} />
+      <DeleteModal />
+      <PublishModal />
     </>
   )
 }
@@ -59,48 +61,48 @@ export function Navbar(props: NavbarProps) {
       <Link to='/' className='flex items-center gap-2'>
         <span className='text-lg font-semibold text-primary'>CV Online</span>
       </Link>
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button
-              variant='outline'
-              size='icon'
-              className='w-fit rounded-full flex gap-2 items-center pl-1 pr-1.5 hover:bg-transparent border border-transparent hover:border-primary/50'
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button
+            variant='outline'
+            size='icon'
+            className='w-fit rounded-full flex gap-2 items-center pl-1 pr-1.5 hover:bg-transparent border border-transparent hover:border-primary/50'
+          >
+            <div className='h-8 w-8 rounded-full bg-gray-100 flex items-center justify-center'>
+              <UserIcon className='w-6 h-6 text-slate-800' />
+            </div>
+            <div className='text-left'>
+              <p className='text-slate-800'>{user || 'user'}</p>
+            </div>
+            <ChevronDown />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align='end' className='px-2 pb-2'>
+          <DropdownMenuItem>
+            <button
+              className='flex gap-2 items-center'
+              onClick={() => {
+                props.setOpen(true)
+              }}
             >
-              <div className='h-8 w-8 rounded-full bg-gray-100 flex items-center justify-center'>
-                <UserIcon className='w-6 h-6 text-slate-800' />
-              </div>
-              <div className='text-left'>
-                <p className='text-slate-800'>{user || 'user'}</p>
-              </div>
-              <ChevronDown />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align='end' className='px-2 pb-2'>
-            <DropdownMenuItem>
-              <button
-                className='flex gap-2 items-center'
-                onClick={() => {
-                  props.setOpen(true)
-                }}
-              >
-                <UserIcon className='h-4 w-4' />
-                <span>Account</span>
-              </button>
-            </DropdownMenuItem>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem
-              className={buttonVariants({
-                variant: 'destructive',
-                className: 'w-full',
-              })}
-            >
-              <button className='flex gap-2 items-center' onClick={onLogout}>
-                <LogOutIcon className='h-4 w-4' />
-                <span>Logout</span>
-              </button>
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
+              <UserIcon className='h-4 w-4' />
+              <span>Account</span>
+            </button>
+          </DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem
+            className={buttonVariants({
+              variant: 'destructive',
+              className: 'w-full',
+            })}
+          >
+            <button className='flex gap-2 items-center' onClick={onLogout}>
+              <LogOutIcon className='h-4 w-4' />
+              <span>Logout</span>
+            </button>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </header>
   )
 }

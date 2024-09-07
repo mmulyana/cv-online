739f53bba00e0560fb8b67e97bc30e92b5e2da38
mmulyana <mulyan.t20@gmail.com>
2024-09-07 21:46:23 +0700

    feat: integrate published cv

--- src/routes/cv/index.tsx
+++ src/routes/cv/index.tsx
@@ -1,5 +1,12 @@
+import { useGetPublishResume } from '@/hooks/api/use-resume'
+import { useParams } from 'react-router-dom'
+
 export default function Page() {
-    return (
-        <div>Resume</div>
-    )
+  const { id } = useParams()
+
+  const { isLoading, data } = useGetPublishResume(Number(id))
+
+  if (isLoading) return <p>Loading</p>
+
+  return <div>{data?.data?.data?.name || '-'}</div>
 }
